9e5fae4d39f763c0f9db2700a157a53cd8dfc3a5
Ihor <dovhanych.ihor@student.uzhnu.edu.ua>
2022-10-31 12:00:00 +0200

    Update Program.cs

--- Lab3/Program.cs
+++ Lab3/Program.cs
@@ -17,28 +17,22 @@ using System.Linq;
 namespace ConsoleApp2
 {
 
-    
+
     class Program
     {
         static void Main(string[] args)
         {
-            Proxy log1 = new Proxy(DateTime.Now, "Andrashko");
-            RealSubject sub = new RealSubject(DateTime.Now, "Putin Died");
-
-
-            log1.Request(DateTime.Now, "Andrashko");
-            log1.Request(DateTime.Now, "Pivo");
-            log1.Request(sub);
-            log1.Request(DateTime.Now, "Andrashko");
+            RealSubject sub = new RealSubject();
+            Proxy log1 = new Proxy(sub);
+            Console.WriteLine(sub.Request(DateTime.Now, "Putin Died"));
+            Console.WriteLine(log1.Request(DateTime.Now, "Andrashko"));
+            Console.WriteLine(log1.Request(DateTime.Now, "Pivo"));
+            Console.WriteLine(log1.Request(DateTime.Now, "Andrashko"));
             log1.Request(DateTime.Now, "Pivo");
             log1.Request(DateTime.Now, "Pivo");
             log1.Request(DateTime.Now, "Putin Died");
             log1.Request(DateTime.Now, "Pivo");
             log1.Request(DateTime.Now, "P#hub");
-            log1.Request(sub);
-            log1.Request(sub);
-            log1.Request(sub);
-
             Console.WriteLine(log1.ShowTopRequests(3));
 
 
@@ -49,54 +43,31 @@ namespace ConsoleApp2
     interface ISubject
     {
         string Request(DateTime date, string request);
-        string Request();
     }
     class RealSubject : ISubject
     {
-        public DateTime date;
-        public string req;
-        public RealSubject(DateTime date, string request = "")
+        public RealSubject()
         {
-            this.date = date;
-            this.req = request;
         }
-        public string Request(DateTime date , string request = "")
+        public string Request(DateTime date, string request = "")
         {
             return $"Date: {date}, Req: {request}, result = //Google //Facebook";
         }
-        public string Request() {
-            return $"Date: {this.date}, Req: {this.req}, result = //Google //Facebook";
-        }
     }
-    class Proxy: ISubject
+    class Proxy : ISubject
     {
         private List<string[]> requestArr = new List<string[]>();
-        public Proxy(DateTime date, string request = "")
-        {
-            
-            this.requestArr.Add(new string[] { date.ToString(), request });
-        }
+        private RealSubject sub;
         public Proxy(RealSubject sub)
         {
-            this.requestArr.Add(new string[] { sub.date.ToString(), sub.req });
+            this.sub = sub;
         }
 
 
         public string Request(DateTime date, string request = "")
         {
-            this.requestArr.Add(new string[] { $"{date}", request });
-            return $"Date: {date}, Req: {request}, result = //Google //Facebook";
-        }
-        public string Request()
-        {
-            return $"Date: {this.requestArr[this.requestArr.Count-1][0]}," +
-                $" Req: {this.requestArr[this.requestArr.Count - 1][1]}, result = //Google //Facebook";
-        }
-        public string Request(RealSubject sub)
-        {
-            this.requestArr.Add(new string[] { sub.date.ToString(), sub.req });
-            return $"Date: {sub.date}," +
-                $" Req: {sub.req}, result = //Google //Facebook";
+            this.requestArr.Add(new string[] { date.ToString(), request});
+            return sub.Request(date,request);
         }
         public string ShowTopRequests(int n)
         {
@@ -104,7 +75,7 @@ namespace ConsoleApp2
             Dictionary<string, int> requestsCount = new Dictionary<string, int>();
             string str = "";
 
-             foreach(string[] el1 in this.requestArr)
+            foreach (string[] el1 in this.requestArr)
             {
                 bool check = true;
                 int counter = 0;
@@ -124,17 +95,18 @@ namespace ConsoleApp2
             requestsCount = requestsCount.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
 
             int i = 0;
-            foreach(var item in requestsCount) { 
-                str += $"#{i + 1} Req count: " + $"{item.Value}" + "; Req name: " + $"{item.Key}"+"\n";
+            foreach (var item in requestsCount)
+            {
+                str += $"#{i + 1} Req count: " + $"{item.Value}" + "; Req name: " + $"{item.Key}" + "\n";
                 i++;
-                if( n == i)
+                if (n == i)
                 {
                     break;
                 }
             }
 
             return str;
-            
+
         }
 
     }
